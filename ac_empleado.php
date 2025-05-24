fbb4ad8b9b6e217110f048c8a326e67f8a5db53f
FernanLeandro <fernanflorez4@gmail.com>
2025-05-23 19:15:58 -0500

    correciones

--- ac_empleado.php
+++ ac_empleado.php
@@ -4,3 +4,14 @@ $empleado = new empleado();
 $empleado->setEmpleadoID($_POST['cod_actualizar']);
 $empleado->consultar();    
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actualizar empleado</title>
+</head>
+<body>
+    
+</body>
+</html>
