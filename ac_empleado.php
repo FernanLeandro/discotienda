09e0d2ce79efa886f4d29a61c814680f3d6a5758
FernanLeandro <fernanflorez4@gmail.com>
2025-05-23 22:34:45 -0500

    batallando con los htas ac_

--- ac_empleado.php
+++ ac_empleado.php
@@ -9,9 +9,28 @@ $empleado->consultar();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Actualizar empleado</title>
+    <title>Actualizar Empleado</title>
 </head>
 <body>
-    
+    <form action="artista/op_actualizar.php" method="POST">
+        <input type="hidden" name="artistaID" value="<?php echo $artista->getArtistaID(); ?>">
+        <div>
+            <label for="nombre">Nombre </label>
+            <input type="text" name="nombre" id="getNombre" class="dato_formulario" required maxlength="200" value="<?php echo $artista->getNombre(); ?>">
+        </div>
+        <div>
+            <label for="nacionalidad">Nacionalidad</label>
+            <input type="text" name="nacionalidad" id="nacionalidad" class="dato_formulario" required value="<?php echo $artista->getNacionalidad(); ?>">
+        </div>
+        <div class="enviar_formulario">
+            <input type="submit" value="Actualizar">
+        </div>
+        <div class="limpiar_formulario">
+            <input type="reset" value="Limpiar datos">
+        </div>
+        <div>
+            <input type="button" value="Volver" onclick="document.location.href='l_artista.php'">
+        </div>
+    </form>
 </body>
 </html>
