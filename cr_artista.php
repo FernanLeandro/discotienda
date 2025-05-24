2221ca4ccc6981fa18d52cde6b8bd21adeebe460
FernanLeandro <fernanflorez4@gmail.com>
2025-05-23 19:59:58 -0500

    se crea arquitectura cr_

--- cr_artista.php
+++ cr_artista.php
@@ -1,15 +1,9 @@
-<?php
-include('class/c_proveedor.php');
-$ = new ();
-$->($_POST['cod_actualizar']);
-$->consultar();    
-?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Crear Artista</title>
 </head>
 <body>
     
