225a62c7269f6942efb476a6f44c7aa811e4cef0
FernanLeandro <fernanflorez4@gmail.com>
2025-05-23 19:45:25 -0500

    se corrige el c_artista, se modifican ac_, y se corrige index

---
+++ cr_artista.php
@@ -0,0 +1,17 @@
+<?php
+include('class/c_proveedor.php');
+$ = new ();
+$->($_POST['cod_actualizar']);
+$->consultar();    
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    
+</body>
+</html>
